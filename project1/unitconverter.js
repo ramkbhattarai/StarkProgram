99e64a15e61a9c1fe50fd321fc326d1e7949be7b
Ram <ramkb466@gmail.com>
2019-10-23 17:21:57 -0400

    worked on the css and added the promt and alert

--- project1/unitconverter.js
+++ project1/unitconverter.js
@@ -6,9 +6,11 @@
 
 let farenheit = document.getElementById('farenheit');
 farenheit.addEventListener("change", () => {
+     prompt("Please enter number.")
     let x = farenheit.value;
     let ans = changeFtC(x);
     document.getElementById("celsius").value = ans;
+    alert("Wait for the answer a moment.")
 });
 
 function changeFtC(n){
@@ -17,9 +19,12 @@ function changeFtC(n){
 
 let celsius = document.getElementById('celsius');
 celsius.addEventListener("change", () => {
+    prompt("Please enter number.")
     let x = celsius.value;
     let ans = changeCTF(x);
     farenheit.value = ans;
+    alert("Wait for the answer a moment.")
+
 });
 
 function changeCTF(n) {
@@ -30,48 +35,60 @@ function changeCTF(n) {
 
 let cups = document.getElementById('cups');
 cups.addEventListener("change", () => {
+    prompt("Please enter number.")
     let x = cups.value;
     let ans = changeCTT(x);
     document.getElementById("teaspoons").value = ans;
+    alert("Wait for the answer a moment.")
+
 });
 
 function changeCTT(n) {
-    return Math.floor(n*48);
+    return n*48;
 }
 
 let teaspoons = document.getElementById('teaspoons');
 teaspoons.addEventListener("change", () => {
+    prompt("Please enter number.")
     let x = teaspoons.value;
     let ans = changeTTC(x);
     cups.value = ans;
+    alert("Wait forthe answer a moment.")
+
 });
 
 function changeTTC(n) {
-    return Math.floor(n/48);
+    return n/48;
 }
 
 
 // this is comment
 let leagues = document.getElementById('leagues');
 leagues.addEventListener("change", () => {
+    prompt("Please enter number.")
     let x = leagues.value;
     let ans = changeLtF(x);
     document.getElementById("forlongs").value = ans;
+    alert("Wait for the answer a moment.")
+
 });
 
 function changeLtF(n) {
-    return Math.floor(n * 27.6187);
+    return n * 27.6187;
 }
 
 let forlongs = document.getElementById('forlongs');
 forlongs.addEventListener("change", () => {
+    prompt("Please enter number.")
     let x = forlongs.value;
     let ans = changeFtL(x);
     leagues.value = ans;
+    alert("Wait for the answer a moment.")
+
 });
 
 function changeFtL(n) {
-    return Math.floor(n / 27.6187);
+    return n / 27.6187;
 }
 
  
